a21e7cf65afa0acd55cdcc6a10758f16ad000ff6
Nekmo <contacto@nekmo.com>
2018-01-12 03:04:15 +0100

    comma_separated_files.

--- dirhunt/management.py
+++ dirhunt/management.py
@@ -13,17 +13,29 @@ def comma_separated(ctx, param, value):
     return (value or '').split(',')
 
 
+def comma_separated_files(ctx, param, value):
+    values = comma_separated(ctx, param, value)
+    items = []
+    for value in values:
+        if value.startswith('/') or value.startswith('./'):
+            lines = [line.rstrip('\r\r') for line in open(value).readlines()]
+            items += [line for line in lines if line]
+        else:
+            items.append(value)
+    return items
+
+
 def status_code_range(start, end):
     return list(filter(lambda x: start <= x <= end, STATUS_CODES))
 
 
 @click.command()
 @click.argument('urls', nargs=-1)
-@click.option('-x', '--exclude-flags', callback=comma_separated,
+@click.option('-x', '--exclude-flags', callback=comma_separated_files,
               help='Exclude results with these flags. See documentation.')
-@click.option('-e', '--interesting-extensions', callback=comma_separated, default=','.join(INTERESTING_EXTS),
+@click.option('-e', '--interesting-extensions', callback=comma_separated_files, default=','.join(INTERESTING_EXTS),
               help='The files found with these extensions are interesting')
-@click.option('-f', '--interesting-files', callback=comma_separated, default=','.join(INTERESTING_FILES),
+@click.option('-f', '--interesting-files', callback=comma_separated_files, default=','.join(INTERESTING_FILES),
               help='The files with these names are interesting')
 def hunt(urls, exclude_flags, interesting_extensions, interesting_files):
     """
