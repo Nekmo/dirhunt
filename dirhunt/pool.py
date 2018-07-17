b0282236fbe93822afd71dc7b5eb82519272326c
Nekmo <contacto@nekmo.com>
2018-07-18 01:46:36 +0200

    Issue #1: Use robots.txt (Python 3.4 support)

--- dirhunt/pool.py
+++ dirhunt/pool.py
@@ -1,9 +1,15 @@
 from concurrent.futures import ThreadPoolExecutor
 
+import os
+
 from dirhunt.exceptions import reraise_with_stack
 
 
 class Pool(ThreadPoolExecutor):
+    def __init__(self, max_workers=None, thread_name_prefix=''):
+        max_workers = max_workers or ((os.cpu_count() or 1) * 5)
+        super(Pool, self).__init__(max_workers=max_workers, thread_name_prefix=thread_name_prefix)
+
     def callback(self, *args, **kwargs):
         raise NotImplementedError
 
