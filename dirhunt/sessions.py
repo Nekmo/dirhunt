1db770d55e8989fcfd3be2178bffe3984f91fd76
Nekmo <contacto@nekmo.com>
2018-08-31 19:17:40 +0200

    Issue #14: Delay between requests

--- dirhunt/sessions.py
+++ dirhunt/sessions.py
@@ -1,30 +1,55 @@
+import random
+import threading
 import warnings
+from queue import Queue
+
 import requests
 
 
+def lock(fn):
+    def wrap(self: 'Session', *args, **kwargs):
+        try:
+            return fn(self, *args, **kwargs)
+        finally:
+            self.sessions.add_available(self)
+    return wrap
+
+
 class Session(object):
-    def __init__(self, sessions):
+    def __init__(self, sessions, proxy):
         self.sessions = sessions
+        self.proxy = proxy
         self.session = requests.Session()
 
+    @lock
     def get(self, url, **kwargs):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             response = self.session.get(url, **kwargs)
-        self.sessions.availables.add(self)
         return response
 
 
 class Sessions(object):
-    def __init__(self, proxies=None):
-        self.availables = set()
+    def __init__(self, proxies=None, delay: int=0):
+        self.availables = Queue()
+        self.delay = delay
+        self.sessions = self.create_sessions(proxies or [None])
+        for session in self.sessions:
+            self.availables.put(session)
+
+    def add_available(self, session):
+        if self.delay:
+            threading.Timer(self.delay, self.availables.put, session)
+        else:
+            self.availables.put(session)
+
+    def create_sessions(self, proxies):
+        return [Session(self, proxy) for proxy in proxies]
+
+    def get_random_session(self):
+        return random.choice(self.sessions)
 
     def get_session(self):
-        if not self.availables:
-            return self.create_session()
-        # self.availables.pop()
-        # Get a element without remove until slots available
-        return next(iter(self.availables))
-
-    def create_session(self):
-        return Session(self)
+        if not self.delay and self.availables.empty():
+            return self.get_random_session()
+        return self.availables.get()
