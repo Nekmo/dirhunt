2398a276076b2d2e94cb3871c71a125d7dec2d40
Nekmo <contacto@nekmo.com>
2018-09-21 00:26:17 +0200

    Issue #2: Search on Google (tests).

--- dirhunt/tests/test_sources.py
+++ dirhunt/tests/test_sources.py
@@ -1,7 +1,10 @@
 import unittest
+from urllib.error import URLError
+
 import requests_mock
 
-from dirhunt.sources import Robots, VirusTotal
+from dirhunt.sources import Robots, VirusTotal, Google
+from dirhunt.sources.google import STOP_AFTER
 from dirhunt.sources.robots import DirhuntRobotFileParser
 from dirhunt.sources.virustotal import VT_URL, ABUSE
 from dirhunt.tests._compat import patch, call
@@ -74,3 +77,25 @@ class TestVirusTotal(unittest.TestCase):
         with patch.object(VirusTotal, 'add_error') as mock_add_error:
             VirusTotal(lambda x: x, lambda x: x).callback(domain)
             mock_add_error.assert_called()
+
+
+class TestGoogle(unittest.TestCase):
+
+    @patch.object(Google, 'add_result')
+    def test_search(self, m1):
+        domain = 'domain'
+        urls = ['http://domain/path1', 'http://domain/path2']
+
+        with patch('dirhunt.sources.google.search', side_effect=lambda x, stop: iter(urls)) as m2:
+            Google(lambda x: x, None).callback(domain)
+            m2.assert_called_once_with('site:{}'.format(domain), stop=STOP_AFTER)
+        m1.assert_has_calls([call(url) for url in urls])
+
+    @patch.object(Google, 'add_error')
+    def test_failure(self, m1):
+        def search_iter(*args):
+            raise URLError('Test')
+
+        with patch('dirhunt.sources.google.search', return_value=map(search_iter, [0])):
+            Google(lambda x: x, lambda x: x).callback('domain')
+        m1.assert_called_once()
