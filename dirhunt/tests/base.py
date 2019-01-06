ca76f97d23e95d332e810ca86e6a1e68e00b959e
Nekmo <contacto@nekmo.com>
2019-01-06 02:35:24 +0100

    Issue #67: Exclude sources from tests

--- dirhunt/tests/base.py
+++ dirhunt/tests/base.py
@@ -1,12 +1,15 @@
 from dirhunt.crawler import Crawler
 from dirhunt.crawler_url import CrawlerUrl
+from dirhunt.sources import SOURCE_CLASSES, get_source_name
 
 
 class CrawlerTestBase(object):
     url = 'http://domain.com/path/'
 
     def get_crawler(self, **kwargs):
-        return Crawler(interesting_extensions=['php'], interesting_files=['error_log'], **kwargs)
+        return Crawler(interesting_extensions=['php'], interesting_files=['error_log'],
+                       exclude_sources=[get_source_name(src) for src in SOURCE_CLASSES],
+                       **kwargs)
 
     def get_crawler_url(self):
          crawler = self.get_crawler()
