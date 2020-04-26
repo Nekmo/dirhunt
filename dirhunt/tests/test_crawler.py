fc563d10f3fe170bc312e132e7418fc7e992a54a
Nekmo <contacto@nekmo.com>
2020-04-27 01:28:12 +0200

    Issue #53: Continue after close Dirhunt (test resume).

--- dirhunt/tests/test_crawler.py
+++ dirhunt/tests/test_crawler.py
@@ -8,12 +8,56 @@ from dirhunt.processors import GenericProcessor
 from dirhunt.tests.base import CrawlerTestBase
 
 
+REPORT_DATA = {
+    "processed": [
+        {
+            "crawler_url": {
+                "depth": 3,
+                "exists": None,
+                "flags": [
+                    "302",
+                    "redirect"
+                ],
+                "type": "directory",
+                "url": {
+                    "address": "https://site.com/",
+                    "domain": "site.com"
+                }
+            },
+            "line": "",
+            "processor_class": "ProcessRedirect",
+            "status_code": 302
+        },
+    ],
+    "processing": [
+        "https://site.com/path/",
+    ]
+}
+
+
 class TestCrawler(CrawlerTestBase, unittest.TestCase):
     def test_print_results(self):
         crawler = self.get_crawler()
         crawler_url = CrawlerUrl(crawler, self.url)
         crawler.results.put(GenericProcessor(None, crawler_url))
         crawler.print_results()
+
+    @patch('dirhunt.crawler.json.dump')
+    def test_create_report(self, m):
+        crawler = self.get_crawler()
+        crawler.results.put(GenericProcessor(None, CrawlerUrl(crawler, self.url)))
+        crawler.create_report(crawler.get_resume_file())
+        m.assert_called_once()
+
+    @patch('dirhunt.crawler.json.load', return_value=REPORT_DATA)
+    @patch('dirhunt.crawler.Crawler.echo', return_value=REPORT_DATA)
+    @patch('dirhunt.crawler.Crawler.add_url', return_value=REPORT_DATA)
+    def test_resume(self, m1, m2, m3):
+        crawler = self.get_crawler()
+        crawler.resume(crawler.get_resume_file())
+        m3.assert_called_once()
+        m2.assert_called_once()
+        m1.assert_called_once_with(REPORT_DATA['processing'][0])
 
     def test_print_results_limit(self):
         crawler = self.get_crawler(limit=1)
