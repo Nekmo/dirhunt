7f1910b62cae305f79729a190e7e8b9ba3668cff
Nekmo <contacto@nekmo.com>
2023-08-08 22:13:35 +0200

    Issue #120: Ignore base64 urls from wayback machine

--- dirhunt/sources/wayback.py
+++ dirhunt/sources/wayback.py
@@ -12,6 +12,7 @@ WAYBACK_PARAMS = {
     'limit': '10000'
 }
 DEFAULT_ENCODING = 'utf-8'
+BASE64_URL = "%22data:%3C;base64"
 TIMEOUT = 10
 
 
@@ -26,6 +27,8 @@ class Wayback(Source):
                 for line in filter(bool, response.iter_lines()):
                     if isinstance(line, bytes):
                         line = line.decode(response.encoding or DEFAULT_ENCODING)
+                    if BASE64_URL in line:
+                        continue
                     self.add_result(line)
         except RequestException as e:
             self.add_error('Error on Wayback source: {}'.format(e))
